409e5f576e34537249e4360fc55959b79fe98ac6
tilzz25 <tilakp1151@gmail.com>
2024-12-10 16:32:17 +0300

    answered question 1

---
+++ question1.js
@@ -0,0 +1,7 @@
+// Question 1: What is JavaScript, and how does it differ from HTML and CSS?
+
+
+// JavaScript is a scripting language used to create interactive web applications.
+// HTML structures the content of a webpage.
+// CSS styles the content.
+
