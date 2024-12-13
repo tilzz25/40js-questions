2eccaae8d1c6c67f2ba62d4101cbd910481b3853
tilzz25 <tilakp1151@gmail.com>
2024-12-13 15:23:53 +0300

    answered question  4

---
+++ question4.js
@@ -0,0 +1,8 @@
+//4)What are the differences between var, let, and const?
+
+//Variables declared with var are hoisted, meaning they can be used before they are declared but he value will be undefined.
+
+//It is not hoisted like var, so you cannot access it before declaration.
+
+//The value assigned to a const variable cannot be reassigned,
+// but the contents of objects or arrays declared with const can still be modified.
