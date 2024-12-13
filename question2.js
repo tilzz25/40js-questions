ac5d85463195251f468cb26ef3372f42adfce7a2
tilzz25 <tilakp1151@gmail.com>
2024-12-13 15:24:12 +0300

    edited

--- question2.js
+++ question2.js
@@ -1,4 +1,4 @@
-// How can you include JavaScript in an HTML file? (Write an example.
+// 2) How can you include JavaScript in an HTML file? (Write an example.
 
 <script>
     console.log("this is how to includea JS file")
