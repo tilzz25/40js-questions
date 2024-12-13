6699059d153c37d532e0a129ee4e798c38f272e3
tilzz25 <tilakp1151@gmail.com>
2024-12-13 15:16:49 +0300

    answered question 3

---
+++ question3.js
@@ -0,0 +1,6 @@
+// 3)What are variables in JavaScript, and how do you declare them?
+
+// a variable is used to store data that can be manipulated througout your code
+
+var name = "tiak";
+console.log(tilak); 
