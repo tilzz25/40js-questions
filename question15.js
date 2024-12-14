7759b1c6298eddce57b5f62eb2562274664797dc
tilzz25 <tilakp1151@gmail.com>
2024-12-14 14:31:53 +0300

    answered QN15

---
+++ question15.js
@@ -0,0 +1,10 @@
+//15) Write a JavaScript expression that evaluates whether a number is both greater than 10 and less than 20.
+
+let number = 18;
+console.log(number > 10 && number < 20); 
+
+number = 25;
+console.log(number > 10 && number < 20); 
+
+number = 10;
+console.log(number > 10 && number < 20); 
