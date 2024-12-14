9d755a7d47f83743e212d5b14eeb1bf5d6ea6b06
tilzz25 <tilakp1151@gmail.com>
2024-12-14 14:13:00 +0300

    answered QN6

---
+++ question6.js
@@ -0,0 +1,15 @@
+// 6) What are data types in JavaScript? List at least three examples.
+
+//Primitive Data Types:
+
+//String: Example: "Hello, World!"
+//Number: Example: 42, 3.14
+//Boolean: Represents logical values, either true or false.
+
+
+//Non-Primitive Data Types:
+
+//Object: Example: { name: "Tilak", age: 25 }
+//Array: Example: [1, 2, 3, 4]
+//Function: Example: function greet() { return "Hello!"; }
+
