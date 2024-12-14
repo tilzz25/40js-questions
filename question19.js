4ea134ab6af53092bef2f4a4287304681f8f2ca5
tilzz25 <tilakp1151@gmail.com>
2024-12-14 14:38:24 +0300

    answered qn19

---
+++ question19.js
@@ -0,0 +1,4 @@
+//19) Explain the concept of scope in JavaScript.
+
+//In JavaScript, scope refers to the context in which variables and functions are accessible or visible.
+// It defines where a variable or function can be referenced and used within the code.
