2f33c0d4071754f60eb8d8a8c52bf685f84a6882
tilzz25 <tilakp1151@gmail.com>
2024-12-14 14:21:42 +0300

    answered QN11

---
+++ question11.js
@@ -0,0 +1,4 @@
+//11) What is the difference between == and === in JavaScript?
+
+// == is used to assign a value to something.
+// === Compares both the value and the type of the operands. Both must be the same for the comparison to return true.
