a993f42089d71a2fb8bfbb2011d0fc5f7b1026d9
tilzz25 <tilakp1151@gmail.com>
2024-12-14 14:27:36 +0300

    answered QN13

---
+++ question13.js
@@ -0,0 +1,10 @@
+//13) List and explain three arithmetic operators in JavaScript.
+
+//1. Addition (+)
+// Adds two numbers.
+
+//2. Subtraction (-)
+// Subtracts the second operand from the first.
+
+//3. Multiplication (*)
+// Multiplies two numbers.
