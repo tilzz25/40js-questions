20b192b2b7d1a612e5e1bca88f40d8b2eb11cd44
tilzz25 <tilakp1151@gmail.com>
2024-12-14 14:45:53 +0300

    answered qn20

---
+++ question20.js
@@ -0,0 +1,5 @@
+//20) What are arrow functions, and how are they different from regular functions?
+
+//Arrow functions are a more concise way to write functions in JavaScript.
+//They provide a shorter syntax compared to regular function expressions and have some key differences,
+// especially in terms of how they handle this.
