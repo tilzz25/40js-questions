91b5ad45f320d419d2887c14554658a36b8aa252
tilzz25 <tilakp1151@gmail.com>
2024-12-14 14:36:44 +0300

    answered qn18

---
+++ question18.js
@@ -0,0 +1,4 @@
+//18) What is the difference between function declarations and function expressions?
+
+//A function declaration is a standard way of defining a function.
+//A function expression involves defining a function and assigning it to a variable.
