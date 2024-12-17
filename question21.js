3e0c326e48a4d827b7f78b4ac0c788b1ef120a17
tilzz25 <tilakp1151@gmail.com>
2024-12-17 21:04:19 +0300

    answered qn21

---
+++ question21.js
@@ -0,0 +1,6 @@
+//21) What is the DOM (Document Object Model) in JavaScript?
+
+//The Document Object Model (DOM) in JavaScript is a programming interface for web documents.
+// It represents the structure of a webpage as a tree-like hierarchy where each element, attribute,
+// and piece of text in the HTML document becomes an object that can be accessed, manipulated,
+// and updated using JavaScript.
