322cdc7e8e68aa4ec7f1337d8fb38b6274db693f
tilzz25 <tilakp1151@gmail.com>
2024-12-21 10:59:05 +0300

    answered qn32

---
+++ question32.js
@@ -0,0 +1,6 @@
+//32) How can you access the third element in the following array?
+//const colors = ["red", "blue", "green", "yellow"];
+
+
+const thirdElement = colors[2];
+console.log(thirdElement);
