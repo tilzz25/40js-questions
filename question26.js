05e9478e469ca49ff92d6a4842d344182ed35668
tilzz25 <tilakp1151@gmail.com>
2024-12-21 10:48:09 +0300

    answered qn26

---
+++ question26.js
@@ -0,0 +1,9 @@
+//26) How do you create an HTML element dynamically using JavaScript?
+
+//1) Create the element: Use document.createElement() to create a new HTML element.
+
+//2)Set its attributes or content: Use properties like id, className, or setAttribute() to add attributes,
+// and innerHTML or textContent to add content.
+
+//3)Append it to the DOM: Use methods like appendChild() or append()
+// to add the new element to the desired location in the DOM.
