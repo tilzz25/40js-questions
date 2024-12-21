449e106f9740db1197345c4909a41ee889198bdb
tilzz25 <tilakp1151@gmail.com>
2024-12-21 10:28:43 +0300

    answered qn24

---
+++ question24.js
@@ -0,0 +1,3 @@
+//24) How do you change the text content of an HTML element using JavaScript?
+
+// You can change the text content of an HTML element in JavaScript using the textContent or innerHTML property of the element.
