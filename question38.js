c539f19b042909b457a3f9aff83aa7b4ba3b2494
tilzz25 <tilakp1151@gmail.com>
2024-12-21 11:13:12 +0300

    answered qn38

---
+++ question38.js
@@ -0,0 +1,7 @@
+//38) What is the difference between a for loop and a while loop?
+
+//For Loop:
+//Used when the number of iterations is known beforehand.
+
+//While Loop:
+//Used when the number of iterations is not known beforehand and depends on a condition.
