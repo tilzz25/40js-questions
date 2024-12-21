46d30110c3b85d57c4de8504920ee86b717ac42d
tilzz25 <tilakp1151@gmail.com>
2024-12-21 11:14:08 +0300

    answered qn39

---
+++ question39.js
@@ -0,0 +1,9 @@
+//39) Write a for...of loop to iterate over the following array:
+//const fruits = ["apple", "banana", "cherry"];
+
+
+const fruits = ["apple", "banana", "cherry"];
+
+for (const fruit of fruits) {
+  console.log(fruit);
+}
